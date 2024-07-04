61f1858c2d3e03cd7cc1e6f83f6c8c6b060e43f4
RodneyCStead <RodCStead@gmail.com>
2024-07-04 17:14:38 -0230

    added approiate comments, and modified code

--- Problem3/CountLetters.java
+++ Problem3/CountLetters.java
@@ -2,26 +2,37 @@ package Problem3;
 
 import java.util.Scanner;
 
+
+// The CountLetters class is used to count the frequency of each letter in a given word.
+
 public class CountLetters {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a word or phrase: ");
-        String input = scanner.nextLine().toUpperCase();
-        int[] letterCounts = new int[26];
+        int[] counts = new int[26];
+        Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < input.length(); i++) {
+        // Get word from user
+        System.out.print("Enter a single word (letters only, please): ");
+        String word = scan.nextLine();
+
+        // Convert to all upper case
+        word = word.toUpperCase();
+
+        // Count frequency of each letter in string
+        for (int i = 0; i < word.length(); i++) {
             try {
-                int index = input.charAt(i) - 'A';
-                letterCounts[index]++;
+                counts[word.charAt(i) - 'A']++;
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("Not a letter: " + input.charAt(i));
-
+                // Initially, do nothing to ignore non-letter characters
+                // Then modify to print a message for non-letter characters
+                System.out.println("Not a letter: " + word.charAt(i));
             }
         }
 
-        for (int i = 0; i < letterCounts.length; i++) {
-            if (letterCounts[i] > 0) {
-                System.out.println((char) (i + 'A') + ": " + letterCounts[i]);
+        // Print frequencies
+        System.out.println();
+        for (int i = 0; i < counts.length; i++) {
+            if (counts[i] != 0) {
+                System.out.println((char)(i + 'A') + ": " + counts[i]);
             }
         }
     }
